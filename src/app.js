4a7cd2111207d5cc381792cf03ea844153e59e35
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-10-18 19:30:26 -0300

    Requisito 1 feito

--- src/app.js
+++ src/app.js
@@ -10,4 +10,24 @@ app.get('/', (_request, response) => {
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
+const connection = require('./connection');
+
+app.use(express.json());
+
+app.get('/products', async (_req, res) => {
+  const [result] = await connection.execute('SELECT * FROM products');
+  res.status(200).json(result);
+});
+
+app.get('/products/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  const [result] = await connection.execute(`SELECT * FROM products WHERE id = ${id}`);
+
+  if (result.length === 0) {
+    const frase = { message: 'Product not found' };
+    res.status(404).json(frase);
+  }
+
+  res.status(200).json(result[0]);
+});
 module.exports = app;
