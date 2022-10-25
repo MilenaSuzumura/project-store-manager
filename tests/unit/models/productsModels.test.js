bf929b71c3ac587203c7097288329072e9a555e7
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-10-25 12:52:11 -0300

    Alguns testes feitos

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -2,95 +2,76 @@ const { expect } = require('chai');
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
+const connection = require('../../../src/connection');
 
 chai.use(sinonChai);
 
-const productsModel = require('../../../src/models/products.models');
+const { productsModel } = require('../../../src/models/index');
 const mocksProducts = require('./mocks/products');
-const { getAll, productId, insertName, updateProduct, deleteProduct } = productsModel;
+// const { getAll, productId, insertName, updateProduct, deleteProduct } = model.productsModel;
 const { products, productIdMock, productInsert, productsUpdate, productsDelete } = mocksProducts;
 
 describe('Testa os models de Products', function () {
   describe('Testa os SELECT de cada função', function () {
 
     it('Testa se retorna todos os produtos de getAll', async function () {
-      const res = {};
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns();
+      sinon.stub(connection, 'execute').resolves([products]);
 
-      sinon.stub(productsModel, 'getAll').resolves({ status: 200, message: products });
+      const result = await productsModel.getAll();
 
-      await getAll({}, res);
-
-      expect(res.status).to.have.been.calledOnceWith(200);
-      expect(res.json).to.have.been.calledOnceWith(products);
+      expect(result).to.be.equal(products);
     });
-
     it('Testa se retorna um produto pelo id usando productId', async function () {
-      const res = {};
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns();
-
-      sinon.stub(productsModel, 'productId')
-        .resolves({ status: 200, message: productIdMock });
-
-      await productId({ params: { id: 1 } }, res);
-
-      expect(res.status).to.have.been.calledOnceWith(200);
-      expect(res.json).to.have.been.calledOnceWith(productIdMock);
-    });
-  });
+          sinon.stub(connection, 'execute').resolves([products[0]]);
+    
+          const result = await productsModel.productId('1');
+    
+          expect(result).to.be.equal(products[0]);
+        });
+      }); 
   
-  describe('Testa os INSERT de cada função', function () {
-    it('Testa se insere um produto novo com InsertName', async function () {
-      const res = {};
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns();
-      const insert = { name: 'Action Figure do Thor' }
+    describe('Testa os INSERT de cada função', function () {
+      it('Testa se insere um produto novo com InsertName', async function () {
+        sinon.stub(connection, 'execute').resolves([{ insertId: 4 }]);
 
-      sinon.stub(productsModel, 'insertName')
-        .resolves({ status: 201, message: productInsert });
+        const result = await productsModel.insertName('Action Figure do Thor');
 
-      await insertName({ params: insert }, res);
-
-      expect(res.status).to.have.been.calledOnceWith(201);
-      expect(res.json).to.have.been.calledOnceWith(productInsert);
+        expect(result.id).to.be.equal(4);
+      });
     });
-  });
 
-  describe('Testa os UPDATE de cada função', function () {
-    it('Testa se dá para atualizar um produto com updateProduct',
-      async function () {
+/*     describe('Testa os UPDATE de cada função', function () {
+      it('Testa se dá para atualizar um produto com updateProduct',
+        async function () {
+          const res = {};
+          res.status = sinon.stub().returns(res);
+          res.json = sinon.stub().returns();
+          const update = { name: 'Machado do Thor' }
+
+          sinon.stub(productsModel, 'updateProduct')
+            .resolves({ status: 200, message: productsUpdate });
+
+          await updateProduct({ params: update }, res);
+
+          expect(res.status).to.have.been.calledOnceWith(200);
+          expect(res.json).to.have.been.calledOnceWith(productsUpdate);
+        });
+    }); */
+
+    describe('Testa o DELETE de cada função', function () {
+      it('Testa se remove um produto com deleteProduct', async function () {
         const res = {};
         res.status = sinon.stub().returns(res);
         res.json = sinon.stub().returns();
-        const update = { name: 'Machado do Thor' }
 
-        sinon.stub(productsModel, 'updateProduct')
-          .resolves({ status: 200, message: productsUpdate });
+        sinon.stub(productsModel, 'deleteProduct')
+          .resolves({ status: 204 });
 
-        await updateProduct({ params: update }, res);
+        await deleteProduct({ params: { id: 1 } }, res);
 
-        expect(res.status).to.have.been.calledOnceWith(200);
-        expect(res.json).to.have.been.calledOnceWith(productsUpdate);
+        expect(res.status).to.have.been.calledOnceWith(204);
+        expect(res.json).to.have.been.calledOnceWith(productsDelete);
       });
+    })
+    afterEach(sinon.restore);
   });
-
-  describe('Testa o DELETE de cada função', function () {
-    it('Testa se remove um produto com deleteProduct', async function () {
-      const res = {};
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns();
-
-      sinon.stub(productsModel, 'deleteProduct')
-        .resolves({ status: 204 });
-
-      await deleteProduct({ params: { id: 1 } }, res);
-
-      expect(res.status).to.have.been.calledOnceWith(204);
-      expect(res.json).to.have.been.calledOnceWith(productsDelete);
-    });
-  })
-
-
-});
