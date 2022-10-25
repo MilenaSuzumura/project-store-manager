0379ebde85ad8e4c85a85073a8215a3ec573bb21
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-10-25 11:58:26 -0300

    Tentei fazer os testes

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -2,51 +2,10 @@ const { expect } = require('chai');
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
-const productsModel = require('../../../src/models/products.models');
-
-chai.use(sinonChai);
-
-describe('Testa os models de Products', function () {
-  describe('Testa os SELECT de cada função', function () {
-
-    it('Testa se retorna todos os produtos de getAll', async function () {
-      const res = {};
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns();
-
-      sinon.stub(productsModel, 'getAll').resolves({ status: 200, message: products });
-
-      await getAll({}, res);
-
-      expect(res.status).to.have.been.calledOnceWith(200);
-      expect(res.json).to.have.been.calledOnceWith(products);
-    });
-
-    it('Testa se retorna um produto pelo id usando productId', async function () {
-      const res = {};
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns();
-
-      sinon.stub(productsModel, 'productId')
-        .resolves({ status: 200, message: productIdMock });
-
-      await productId({ params: { id: 1 } }, res);
-
-      expect(res.status).to.have.been.calledOnceWith(200);
-      expect(res.json).to.have.been.calledOnceWith(productIdMock);
-    });
-  });
-});
-
-/* const { expect } = require('chai');
-const chai = require('chai');
-const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
 
 chai.use(sinonChai);
 
 const productsModel = require('../../../src/models/products.models');
-const { productsDelete } = require('./mocks/products');
 const mocksProducts = require('./mocks/products');
 const { getAll, productId, insertName, updateProduct, deleteProduct } = productsModel;
 const { products, productIdMock, productInsert, productsUpdate, productsDelete } = mocksProducts;
@@ -134,4 +93,4 @@ describe('Testa os models de Products', function () {
   })
 
 
-}); */
+});
