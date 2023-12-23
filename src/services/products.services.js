157d431a25d59ca079ba58e140358063ba949575
Milena Suzumura <milena.suzumura11@gmail.com>
2023-12-22 21:45:59 -0300

    fix: Agora a função updateProduct funciona

--- src/services/products.services.js
+++ src/services/products.services.js
@@ -32,13 +32,13 @@ const createProduct = async (name) => {
 const updateProduct = async (id, name) => {
   const productId = await productsModel.productId(id);
   const validationId = validationProductId(productId);
-  
-  if (validationId) {
+
+  if (validationId.status === 404) {
     return validationId;
   }
 
   const validationName = validationProductName(name);
-  
+
   if (validationName) {
     return validationName;
   }
@@ -49,6 +49,7 @@ const updateProduct = async (id, name) => {
     name,
   };
 
+  console.log(product)
   return {
     status: 200,
     message: product,
